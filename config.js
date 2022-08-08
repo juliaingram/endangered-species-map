a9c657101882da40ce2d047419ff47d0daf01516
Susan Merriam <44954772+semerriam@users.noreply.github.com>
2022-08-08 10:32:19 -0400

    Update config.js

--- config.js
+++ config.js
@@ -51,7 +51,8 @@ let divChapter3 =
 +
    "<br><img src='images/flies2.png'>"
 + 
-   "<div class=core-html'><p class='viz-caption'>Images: Joe Giersch, USGS</p></div>"
+//    "<div class=core-html'><p class='viz-caption'>Images: Joe Giersch, USGS</p></div>"
+   "<p class='viz-caption'>Images: Joe Giersch, USGS</p>"
 ;
 
 let divChapter4 = 
